1c132179921e443d2e4b64683913c179b9a082cf
Poliakk <poliakovajv@gmail.com>
2023-10-20 11:05:28 +0300

    Calculator 1) DZ2_lesson3

---
+++ src/Calculator.java
@@ -0,0 +1,34 @@
+/*
+ DZ2
+ Необходимо написать простой консольный калькулятор на Java.
+ Для считывания ввода нужно использовать класс Scanner. В конце напечатать результат.
+ Используйте форматированный вывод. Пока пусть работает так, пользователь вводит целые числа,
+ а на вывод передается результат 4-х операций(+, -, *, /) без выбора операции.
+ В дальнейшем будете дорабатывать приложение.
+*/
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        // Объявление переменных
+        int firstNumber, secondNumber, sum, diff, product, quotient;
+        // Запрос данных пользователя
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        firstNumber = scanner.nextInt();
+        System.out.println("Введите второе число");
+        secondNumber = scanner.nextInt();
+        scanner.close();
+        // Операции
+        sum = firstNumber + secondNumber;
+        diff = firstNumber - secondNumber;
+        product = firstNumber * secondNumber;
+        quotient = firstNumber / secondNumber;
+        // Вывод результатов операций (+, -, *, /)
+        System.out.printf("\tРезультат:\nСложение:\n" + firstNumber + " + " + secondNumber + " = %d", sum);
+        System.out.printf("\nВычитание:\n" + firstNumber + " - " + secondNumber + " = %d", diff);
+        System.out.printf("\nУмножение:\n" + firstNumber + " * " + secondNumber + " = %d", product);
+        System.out.printf("\nДеление:\n" + firstNumber + " / " + secondNumber + " = %d", quotient);
+    }
+}
