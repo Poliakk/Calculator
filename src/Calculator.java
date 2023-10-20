e68fd8769861d36264385cd5bd7c8a336e0e06cb
Poliakk <poliakovajv@gmail.com>
2023-10-21 00:31:58 +0300

    2) DZ3.1_lesson4 добавлено меню выбора операции в консоли, использованы конструкции switch и while.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,11 @@
 /*
- DZ2
- Необходимо написать простой консольный калькулятор на Java.
- Для считывания ввода нужно использовать класс Scanner. В конце напечатать результат.
- Используйте форматированный вывод. Пока пусть работает так, пользователь вводит целые числа,
- а на вывод передается результат 4-х операций(+, -, *, /) без выбора операции.
- В дальнейшем будете дорабатывать приложение.
+ DZ2: Необходимо написать простой консольный калькулятор на Java. Для считывания ввода нужно использовать класс Scanner.
+ В конце напечатать результат. Используйте форматированный вывод.
+ Пока пусть работает так, пользователь вводит целые числа,а на вывод передается результат 4-х операций(+, -, *, /)
+ без выбора операции.В дальнейшем будете дорабатывать приложение.
+ DZ3.1: Доработать калькулятор, получение от пользователя действия, которое нужно выполнить.
+ Можно получить таким образом - если пользователь вводит 1, то это сложение, 2 - разность и т.д.
+ Вывести результат действия. Используйте конструкции if -else, case.
 */
 
 import java.util.Scanner;
@@ -12,24 +13,43 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         // Объявление переменных
-        int firstNumber, secondNumber, sum, diff, product;
+        int firstNumber, secondNumber, command = 1, sum, diff, product;
         double quotient;
         // Запрос данных пользователя
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число");
+        System.out.print("Введите первое число: ");
         firstNumber = scanner.nextInt();
-        System.out.println("Введите второе число");
+        System.out.print("Введите второе число: ");
         secondNumber = scanner.nextInt();
-        scanner.close();
         // Операции
-        sum = firstNumber + secondNumber;
-        diff = firstNumber - secondNumber;
-        product = firstNumber * secondNumber;
-        quotient = (double) firstNumber / secondNumber;
-        // Вывод результатов операций (+, -, *, /)
-        System.out.printf("\tРезультат:\nСложение:\n" + firstNumber + " + " + secondNumber + " = %d", sum);
-        System.out.printf("\nВычитание:\n" + firstNumber + " - " + secondNumber + " = %d", diff);
-        System.out.printf("\nУмножение:\n" + firstNumber + " * " + secondNumber + " = %d", product);
-        System.out.printf("\nДеление:\n" + firstNumber + " / " + secondNumber + " = %.2f", quotient);
+        while (command !=0) {
+            System.out.println("\nКакое действие выполнить?\n1 - Вычислить сумму\n2 - Вычислить разность\n" +
+                    "3 - Вычислить произведение\n4 - Вычислить частное\n0 - Выйти из приложения");
+            command = scanner.nextInt();
+            switch (command) {
+                case 1:
+                    sum = firstNumber + secondNumber;
+                    System.out.printf("Сумма: " + firstNumber + " + " + secondNumber + " = %d\n", sum);
+                    break;
+                case 2:
+                    diff = firstNumber - secondNumber;
+                    System.out.printf("Разность: " + firstNumber + " - " + secondNumber + " = %d\n", diff);
+                    break;
+                case 3:
+                    product = firstNumber * secondNumber;
+                    System.out.printf("Произведение: " + firstNumber + " * " + secondNumber + " = %d\n", product);
+                    break;
+                case 4:
+                    quotient = (double) firstNumber / secondNumber;
+                    System.out.printf("Частное: " + firstNumber + " / " + secondNumber + " = %.2f\n", quotient);
+                    break;
+                case 0:
+                    System.out.println("Приложение закрыто");
+                    break;
+                default:
+                    System.out.println("Несуществующая команда. Выберите из предложенных вариантов.");
+            }
+        }
+        scanner.close();
     }
 }
